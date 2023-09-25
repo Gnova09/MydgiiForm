168803095414622524e7eb11a8e148e094d2a80e
Georges Bueno <georgs.bueno@hotmail.com>
2023-09-25 12:15:40 -0400

    agregando get fact

--- app/pages/home/factura/page.js
+++ app/pages/home/factura/page.js
@@ -3,8 +3,8 @@ import React, { useState } from 'react'
  import Ptable from '../../../components/ptable'
 import Link from 'next/link'
 import useAppContext from '@/app/context/context'
-/*import { getFacturas } from '@/app/db/controllers/facturas';
-import generarFactura from '@/app/helpers/generarFactura'; */
+import { getFacturas } from '@/app/db/controllers/facturas';
+/*import generarFactura from '@/app/helpers/generarFactura'; */
 
 /* const handleRowButton = async (row) => {
 
