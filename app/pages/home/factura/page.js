f614cb76edd8b59f90f89f22fc822db9a015f77c
Georges Bueno <georgs.bueno@hotmail.com>
2023-09-25 12:19:51 -0400

    added if to generarfactura

--- app/pages/home/factura/page.js
+++ app/pages/home/factura/page.js
@@ -1,14 +1,15 @@
 "use client"
 import React, { useState } from 'react'
- import Ptable from '../../../components/ptable'
+import Ptable from '../../../components/ptable'
 import Link from 'next/link'
 import useAppContext from '@/app/context/context'
 import { getFacturas } from '@/app/db/controllers/facturas';
 import generarFactura from '@/app/helpers/generarFactura';
 
-/* const handleRowButton = async (row) => {
-
-    generarFactura(row)
+const handleRowButton = async (row) => {
+    if (window !== (undefined)) {
+        generarFactura(row)
+    }
 
 }
 const column = [
@@ -35,27 +36,27 @@ const column = [
         ),
     },
 
-] */
+]
 
 export default async function Pages() {
-   /*  const { user } = useAppContext()
+    const { user } = useAppContext()
     const [row, setRow] = useState([])
 
 
     const fetchData = async () => {
         const rowData = await getFacturas(user.uid);
         setRow(rowData);
-    }; */
+    };
 
     return (
         <section class="flex mt-14 bg-white min-h-screen flex-col items-center justify-center">
             <h1 className=' font-extrabold text-3xl m-5'>Facturas</h1>
-{/* 
-            <Ptable callback={() => fetchData()} column={column} row={row} /> 
+
+            <Ptable callback={() => fetchData()} column={column} row={row} />
 
             <Link href="/pages/home/factura/new" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
                 Agregar Factura
-            </Link> */}
+            </Link>
         </section>
     )
 }
