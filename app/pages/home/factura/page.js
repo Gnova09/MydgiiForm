4c45c05b8973724876177490f50397ebf31d43ca
Georges Bueno <georgs.bueno@hotmail.com>
2023-09-25 11:55:56 -0400

    changes to production2

--- app/pages/home/factura/page.js
+++ app/pages/home/factura/page.js
@@ -51,7 +51,7 @@ export default function Pages() {
         <section class="flex mt-14 bg-white min-h-screen flex-col items-center justify-center">
             <h1 className=' font-extrabold text-3xl m-5'>Facturas</h1>
 
-           {/* <Ptable callback={() => fetchData()} column={column} row={row} /> */}
+            <Ptable callback={() => fetchData()} column={column} row={row} /> 
 
             <Link href="/pages/home/factura/new" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
                 Agregar Factura
