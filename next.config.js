24209fd11dd6b97fe302fc552b88393c37e8998e
Georges Bueno <georgs.bueno@hotmail.com>
2023-09-27 10:38:12 -0400

    back to next public env

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        apiKey: `${process.env.apiKey}`,
-        databaseURL: `${process.env.databaseURL}`,
-        authDomain: `${process.env.authDomain}`,
-        projectId: `${process.env.projectId}`,
-        storageBucket: `${process.env.storageBucket}`,
-        messagingSenderId: `${process.env.messagingSenderId}`,
-        appId: `${process.env.appId}`,
-        measurementId: `${process.env.measurementId}`
-    }
+   
 }
 
 module.exports = nextConfig
