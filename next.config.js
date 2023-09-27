15901fc47cf447ff79d7efe2406febbf93b22da8
Georges Bueno <georgs.bueno@hotmail.com>
2023-09-27 10:28:42 -0400

    in next config add env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        apiKey: `${process.env.apiKey}`,
+        databaseURL: `${process.env.databaseURL}`,
+        authDomain: `${process.env.authDomain}`,
+        projectId: `${process.env.projectId}`,
+        storageBucket: `${process.env.storageBucket}`,
+        messagingSenderId: `${process.env.messagingSenderId}`,
+        appId: `${process.env.appId}`,
+        measurementId: `${process.env.measurementId}`
+    }
+}
 
 module.exports = nextConfig
